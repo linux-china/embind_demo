9dbce96d6037668f195b3efb90245702c99b7089
linux_china <libing.chen@gmail.com>
2019-10-09 15:48:39 -0700

    polishing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Module = require('./dist/a-node');
-const wasm = Module({wasmBinaryFile: 'dist/a-node.wasm'});
+const Module = require('./dist/a_node');
+const wasm = Module({wasmBinaryFile: 'dist/a_node.wasm'});
 wasm.onRuntimeInitialized = function() {
     console.log(wasm.exclaim("Jackie"));
 };
