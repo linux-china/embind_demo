296e857c9244a09a40ba73c09aea61a2357dd4c8
linux_china <libing.chen@gmail.com>
2019-10-09 20:04:25 -0700

    add global

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+global["user"] = {
+    findUserInfo: function (id) {
+        console.log("user id:" + id);
+        console.log("call from stubs by wasm");
+        return id + 1
+    }
+};
 const Module = require('./dist/a_node');
 const wasm = Module({wasmBinaryFile: 'dist/a_node.wasm'});
-wasm.onRuntimeInitialized = function() {
+
+wasm.onRuntimeInitialized = function () {
     console.log(wasm.exclaim("Jackie"));
 };
