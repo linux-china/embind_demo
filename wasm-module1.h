42f034ffb24cc40d3698b8a72b630d87caa617ce
linux_china <libing.chen@gmail.com>
2019-10-10 14:39:40 -0700

    make code only

--- wasm-module1.h
+++ wasm-module1.h
@@ -1,27 +1,13 @@
 #ifndef EMBIND_DEMO_WASM_MODULE1_H
 #define EMBIND_DEMO_WASM_MODULE1_H
 
-#include "reactive.h"
-
-#include <emscripten/bind.h>
-
-using namespace emscripten;
-
-//global counter
-int counter = 0;
+#include <string>
 
 double add(double a, double b);
 
 int getCounter();
 
 std::string exclaim(std::string message);
-
-EMSCRIPTEN_BINDINGS(my_module) {
-    function("getCounter", &getCounter);
-    function("add", &add);
-    function("exclaim", &exclaim);
-    function("receive", &receive);
-}
 
 //external api
 std::string findNickById(int i);
