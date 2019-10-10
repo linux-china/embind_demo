91cf72ca5d4dd7a35084cb6b018687a5e1ac2ea5
linux_china <libing.chen@gmail.com>
2019-10-10 14:40:41 -0700

    embind cpp

---
+++ binding.cpp
@@ -0,0 +1,18 @@
+//
+// embind cpp
+//
+#include <emscripten/bind.h>
+
+#include "reactive.h"
+#include "wasm-module1.h"
+
+using namespace emscripten;
+
+EMSCRIPTEN_BINDINGS(my_module) {
+    function("getCounter", &getCounter);
+    function("add", &add);
+    function("exclaim", &exclaim);
+    function("connect", &connect);
+    function("send", &send);
+    function("sendOne", &sendOne);
+}
