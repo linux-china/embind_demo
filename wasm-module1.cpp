bf22920227d6fcb2f0877cc8913a2f163438d4e0
linux_china <libing.chen@gmail.com>
2019-10-09 20:18:13 -0700

    add module

--- wasm-module1.cpp
+++ wasm-module1.cpp
@@ -1,10 +1,17 @@
 #include "wasm-module1.h"
+#include <emscripten/val.h>
+
+std::string findNickById(int i) {
+    val user = val::global("user");
+    return user.call<std::string>("funNickById", val(i));
+}
 
 double add(double a, double b) {
     return a + b;
 }
 
 std::string exclaim(std::string message) {
-    int i = user_findUserInfo(1);
-    return message + "! " + std::to_string(i);
+    val user = val::global("user");
+    // int i = user_findUserInfo(1);
+    return message + "! " + findNickById(1);
 }
