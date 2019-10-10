70094ab5d2dfeb4774d2903577ac6199d0932cfe
linux_china <libing.chen@gmail.com>
2019-10-09 17:21:53 -0700

    adjust string

--- hello.cpp
+++ hello.cpp
@@ -6,19 +6,13 @@
 #include <string>
 
 
-char *Hello::hello(char *) {
-    char *ch = new char[11] /* 11 = len of Hello Heap + 1 char for \0*/;
-    strcpy(ch, "Hello Heap");
-    return ch;
+char *Hello::hello(char *name) {
+    std::string str_name(name);
+    std::string str = "Hello " + str_name;
+    return const_cast<char *>(str.c_str());
 }
 
 int Hello::sum(int a, int b) {
-    //std::string str_name(name);
-    //std::string str = "Hello ";
-    //return const_cast<char *>(str.c_str());
-    //char* ch = new char[11] /* 11 = len of Hello Heap + 1 char for \0*/;
-    //strcpy(ch, "Hello Heap");
-    //return ch;
     return a + b;
 
 }
